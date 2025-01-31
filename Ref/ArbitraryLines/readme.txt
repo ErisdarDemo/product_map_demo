a9fa38d2486f730f9a5fb293b5d3897cc99d0961
Justin Reina <justinmreina@gmail.com>
2025-01-30 18:19:17 -0800

    (+)    populate reference project & starting code        - add workspace & project        - relocate main        - move diagram to system        - add style.h & abitrary lines reference        - compiles and runs

---
+++ Ref/ArbitraryLines/readme.txt
@@ -0,0 +1,14 @@
+@brief 		ArbitraryLines
+@details 	Margin lines in IDE
+@auth 		Justin Reina
+@date 		1/30/25
+
+
+@section 	Notes
+	
+	Aligned with 4-space tabbing
+	
+	- inline comment lead: 52
+	
+	- inline commend end: 100 (page width)
+
---
+++ main.c
@@ -0,0 +1,36 @@
+/**************************************************************************************************/
+/** @file     main.c
+ *  @brief    Troll Product Map Demo
+ *  @details  x
+ *
+ *  @author   Justin Reina, Firmware Engineer
+ *  @created  1/30/25
+ *  @last rev 1/30/25
+ *
+ *
+ *  @section    Opens
+ *      lots
+ *
+ *  @section    Legal Disclaimer
+ *      © 2025 Justin Reina, All rights reserved. All contents of this source file and/or any other
+ *      related source files are for public redistribution at this time.
+ */
+/**************************************************************************************************/
+
+//Standard Library Includes
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**************************************************************************************************/
+/** @fcn        int main(void)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+int main(void) {
+
+	puts("!!!Hello World!!!");                        /* prints !!!Hello World!!!                   */
+
+	return EXIT_SUCCESS;
+}
