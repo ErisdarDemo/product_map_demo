85c4e888b85b417c121beb63289d49ad5dbd0a9f
Justin Reina <justinmreina@gmail.com>
2025-02-02 12:41:01 -0800

    (+)    complete stm32 bare-metal demo integration

--- Cube/Core/Src/main.c
+++ Cube/Core/Src/main.c
@@ -1,78 +1,115 @@
-/**************************************************************************************************/
-/** @file     main.c
- *  @brief    Main program body
- *  @details  x
- *
- *  @author   Justin Reina, Firmware Engineer
- *  @created  2/1/25
- *  @last rev 2/1/25
- *
- *
- *  @notes    x
- *
- *  @section    Opens
- *      none current
- *
- *  @section    Legal Disclaimer
-    *                  � 2025 Justin Reina, All rights reserved. All contents of this source file and/or any other
- *      related source files are the explicit property of Justin Reina. Do not distribute.
- *      Do not copy.
- */
-/**************************************************************************************************/
-
-
-
-//Includes
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
-//Private variables
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart2;
 
+/* USER CODE BEGIN PV */
 
-//Private function prototypes
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
 
+/* USER CODE END PFP */
 
-/**************************************************************************************************/
-/** @fcn        int main(void)
- *  @brief      The application entry point
- *  @details    x
- *
- *  @return   (int) routine status
- */
-/**************************************************************************************************/
-int main(void) {
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
 
-  /* MCU Configuration----------------------------------------------------------------------------*/
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
   /* Configure the system clock */
   SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
 
   /* Infinite loop */
-  for(;;) {
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
 
+    /* USER CODE BEGIN 3 */
   }
+  /* USER CODE END 3 */
 }
 
-
-/**************************************************************************************************/
-/** @fcn        void SystemClock_Config(void)
- *  @brief      x
- *  @details    x
- */
-/**************************************************************************************************/
-void SystemClock_Config(void) {
-
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
@@ -84,75 +121,79 @@ void SystemClock_Config(void) {
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM            = 16;
-  RCC_OscInitStruct.PLL.PLLN            = 336;
-  RCC_OscInitStruct.PLL.PLLP            = RCC_PLLP_DIV4;
-  RCC_OscInitStruct.PLL.PLLQ            = 2;
-  RCC_OscInitStruct.PLL.PLLR            = 2;
-
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
   */
-  RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                                   | RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-
-  RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
     Error_Handler();
   }
-
-  return;
 }
 
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
 
-/**************************************************************************************************/
-/** @fcn        static void MX_USART2_UART_Init(void)
- *  @brief      USART2 Initialization Function
- *  @details    x
- */
-/**************************************************************************************************/
-static void MX_USART2_UART_Init(void) {
+  /* USER CODE BEGIN USART2_Init 0 */
 
-  huart2.Instance          = USART2;
-  huart2.Init.BaudRate     = 115200;
-  huart2.Init.WordLength   = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits     = UART_STOPBITS_1;
-  huart2.Init.Parity       = UART_PARITY_NONE;
-  huart2.Init.Mode         = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-
-  if(HAL_UART_Init(&huart2) != HAL_OK) {
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
     Error_Handler();
   }
+  /* USER CODE BEGIN USART2_Init 2 */
 
-  return;
+  /* USER CODE END USART2_Init 2 */
+
 }
 
-
-/**************************************************************************************************/
-/** @fcn        static void MX_GPIO_Init(void)
- *  @brief      GPIO Initialization Function
- *  @details    x
- */
-/**************************************************************************************************/
-static void MX_GPIO_Init(void) {
-
-  //Locals
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -164,36 +205,54 @@ static void MX_GPIO_Init(void) {
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin  = B1_Pin;
+  GPIO_InitStruct.Pin = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-
   /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin   = LD2_Pin;
-  GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull  = GPIO_NOPULL;
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
-  return;
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
+/* USER CODE BEGIN 4 */
 
-/**************************************************************************************************/
-/** @fcn        void Error_Handler(void)
- *  @brief      This function is executed in case of error occurrence.
- *  @details    x
- */
-/**************************************************************************************************/
-void Error_Handler(void) {
+/* USER CODE END 4 */
 
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-
-  for(;;);
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
 }
 
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
--- main.c
+++ main.c
@@ -5,7 +5,7 @@
  *
  *  @author   Justin Reina, Firmware Engineer
  *  @created  1/30/25
- *  @last rev 1/30/25
+ *  @last rev 2/2/25
  *
  *
  *  @section    Opens
@@ -23,6 +23,114 @@
  *      related source files are for public redistribution at this time.
  */
 /**************************************************************************************************/
+
+//Standard Library Includes
+#include <stdint.h>
+
+
+//************************************************************************************************//
+//                                           DEFINITIONS                                          //
+//************************************************************************************************//
+
+#define NVIC_PRIORITYGROUP_4         (0)
+#define TICK_INT_PRIORITY            (0)
+#define B1_Pin                       (GPIO_PIN_13)
+#define B1_GPIO_Port                 (GPIOC)
+#define USART_TX_Pin                 (GPIO_PIN_2)
+#define USART_TX_GPIO_Port           (GPIOA)
+#define USART_RX_Pin                 (GPIO_PIN_3)
+#define USART_RX_GPIO_Port           (GPIOA)
+#define LD2_Pin                      (GPIO_PIN_5)
+#define LD2_GPIO_Port                (GPIOA)
+#define TMS_Pin                      (GPIO_PIN_13)
+#define TMS_GPIO_Port                (GPIOA)
+#define TCK_Pin                      (GPIO_PIN_14)
+#define TCK_GPIO_Port                (GPIOA)
+#define SWO_Pin                      (GPIO_PIN_3)
+#define SWO_GPIO_Port                (GPIOB)
+#define RCC_OSCILLATORTYPE_HSI       (0x00000002U)
+#define RCC_HSICALIBRATION_DEFAULT   (0x10U)
+#define RCC_HSI_ON                   ((uint8_t)0x01)
+#define RCC_CLOCKTYPE_SYSCLK         (0x00000001U)
+#define RCC_CLOCKTYPE_HCLK           (0x00000002U)
+#define RCC_CLOCKTYPE_PCLK1          (0x00000004U)
+#define RCC_CLOCKTYPE_PCLK2          (0x00000008U)
+#define RCC_CFGR_SW_PLL              (0x00000002U)
+#define RCC_SYSCLKSOURCE_PLLCLK      (RCC_CFGR_SW_PLL)
+#define RCC_CFGR_HPRE_DIV1           (0x00000000U)
+#define RCC_CFGR_PPRE1_DIV1          (0x00000000U)
+#define RCC_CFGR_PPRE1_DIV2          (0x00001000U)
+#define RCC_SYSCLK_DIV1              (RCC_CFGR_HPRE_DIV1)
+#define RCC_HCLK_DIV1                (RCC_CFGR_PPRE1_DIV1)
+#define RCC_HCLK_DIV2                (RCC_CFGR_PPRE1_DIV2)
+
+
+//************************************************************************************************//
+//                                             TYPEDEFS                                           //
+//************************************************************************************************//
+typedef enum {
+  HAL_OK       = 0x00U,
+  HAL_ERROR    = 0x01U,
+  HAL_BUSY     = 0x02U,
+  HAL_TIMEOUT  = 0x03U
+} HAL_StatusTypeDef;
+
+
+typedef struct {
+  uint32_t OscillatorType;       // The oscillators to be configured.
+  uint32_t HSEState;             // The new state of the HSE.
+  uint32_t LSEState;             // The new state of the LSE.
+  uint32_t HSIState;             // The new state of the HSI.
+  uint32_t HSICalibrationValue;  // The HSI calibration trimming value
+  uint32_t LSIState;             // The new state of the LSI.
+} RCC_OscInitTypeDef;
+
+
+typedef struct {
+  uint32_t ClockType;             // The clock to be configured.
+  uint32_t SYSCLKSource;          // The clock source (SYSCLKS) used as system clock.
+  uint32_t AHBCLKDivider;         // The AHB clock (HCLK) divider
+  uint32_t APB1CLKDivider;        // The APB1 clock (PCLK1) divider
+  uint32_t APB2CLKDivider;        // The APB2 clock (PCLK2) divider
+} RCC_ClkInitTypeDef;
+
+
+
+
+//************************************************************************************************//
+//                                            VARIABLE                                            //
+//************************************************************************************************//
+
+//Private variables
+void *huart2;
+
+
+
+//************************************************************************************************//
+//                                          CUBE MX SOURCE                                        //
+//************************************************************************************************//
+
+//HAL Prototypes
+HAL_StatusTypeDef HAL_Init(void);
+
+void Error_Handler(void);
+
+void __HAL_RCC_PWR_CLK_ENABLE(void);
+void __HAL_PWR_VOLTAGESCALING_CONFIG(void);
+
+HAL_StatusTypeDef HAL_RCC_OscConfig(RCC_OscInitTypeDef  *RCC_OscInitStruct);
+HAL_StatusTypeDef HAL_RCC_ClockConfig(RCC_ClkInitTypeDef  *RCC_ClkInitStruct);
+
+
+//Private function prototypes
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+
+
+//************************************************************************************************//
+//                                            DEMO SOURCE                                         //
+//************************************************************************************************//
 
 //Standard Library Includes
 #include <stdio.h>
@@ -61,19 +169,43 @@ static void wifi_init(void);
 /**************************************************************************************************/
 int main(void) {
 
+  //Notify
+	println("Begin Demo! -\r\n");
+
+	//------------------------------------------ HAL Init ------------------------------------------//
+	//Init HAL
+  HAL_Init();             /* Reset of all periphs, Init Flash interface and the Systick. */
+
+  //Init Clock
+  SystemClock_Config();   /* Configure the system clock */
+
+  //Init Periphs
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+
+
+  //----------------------------------------- Demo Init ------------------------------------------//
+
   //Init
   os_init();
 
   //Notify
   printf("Begin Demo -\r\n\r\n");
 
-  //Demo
+
+  //-------------------------------------------- Demo --------------------------------------------//
+
+  //Operate
   wifi_task();
   pmu_task();
   data_task();
   system_task();
 
-	return EXIT_SUCCESS;
+
+  //Notify
+  println("\r\nDemo complete.");
+
+  return EXIT_SUCCESS;
 }
 
 
@@ -271,5 +403,178 @@ static void wifi_init(void) {
   println("WiFi Init");                             /* Share a clean message                      */
 
   return;
+}
+
+//************************************************************************************************//
+//                                          CUBEMX SOURCE                                         //
+//************************************************************************************************//
+
+/**************************************************************************************************/
+/** @fcn        static void MX_GPIO_Init(void)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+static void MX_GPIO_Init(void) {
+  volatile int b = 0;
+  b++;
+  return;
+}
+
+/**************************************************************************************************/
+/** @fcn        static void MX_USART2_UART_Init(void)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+static void MX_USART2_UART_Init(void) {
+  volatile int c = 0;
+  c++;
+  return;
+}
+
+
+/**************************************************************************************************/
+/** @fcn        void SystemClock_Config(void)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+void SystemClock_Config(void) {
+
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG();
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK) {
+    Error_Handler();
+  }
+
+  return;
+}
+
+
+/**************************************************************************************************/
+/** @fcn        void __HAL_RCC_PWR_CLK_ENABLE(void)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+void __HAL_RCC_PWR_CLK_ENABLE(void) {
+  volatile int x = 0;
+  x++;
+  return;
+}
+
+/**************************************************************************************************/
+/** @fcn        void __HAL_PWR_VOLTAGESCALING_CONFIG(void)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+void __HAL_PWR_VOLTAGESCALING_CONFIG(void)  {
+  volatile int y = 0;
+  y++;
+  return;
+}
+
+/**************************************************************************************************/
+/** @fcn        void Error_Handler(void)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+void Error_Handler(void) {
+  volatile int a = 0;
+  a++;
+  return;
+}
+
+
+//************************************************************************************************//
+//                                      STM32F4 HAL_RCC SOURCE                                    //
+//************************************************************************************************//
+
+/**************************************************************************************************/
+/** @fcn        HAL_StatusTypeDef HAL_RCC_OscConfig(RCC_OscInitTypeDef  *RCC_OscInitStruct)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+HAL_StatusTypeDef HAL_RCC_OscConfig(RCC_OscInitTypeDef  *RCC_OscInitStruct) {
+  return HAL_OK;
+}
+
+/**************************************************************************************************/
+/** @fcn        HAL_StatusTypeDef HAL_RCC_ClockConfig(RCC_ClkInitTypeDef  *RCC_ClkInitStruct)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+HAL_StatusTypeDef HAL_RCC_ClockConfig(RCC_ClkInitTypeDef  *RCC_ClkInitStruct) {
+  return HAL_OK;
+}
+
+
+//************************************************************************************************//
+//                                        STM32F4 HAL SOURCE                                      //
+//************************************************************************************************//
+
+  //ST Micro HAL Support
+  void __HAL_FLASH_INSTRUCTION_CACHE_ENABLE(void) { return; }
+  void __HAL_FLASH_DATA_CACHE_ENABLE(void)        { return; }
+  void __HAL_FLASH_PREFETCH_BUFFER_ENABLE(void)   { return; }
+  void HAL_NVIC_SetPriorityGrouping(int x)        { return; }
+  void HAL_InitTick(int x)                        { return; }
+  void HAL_MspInit(void)                          { return; }
+
+
+/**************************************************************************************************/
+/** @fcn        HAL_StatusTypeDef HAL_Init(void)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+HAL_StatusTypeDef HAL_Init(void) {
+
+  __HAL_FLASH_INSTRUCTION_CACHE_ENABLE();
+  __HAL_FLASH_DATA_CACHE_ENABLE();
+  __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
+
+  /* Set Interrupt Group Priority */
+  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+
+  /* Use systick as time base source and configure 1ms tick */
+  HAL_InitTick(TICK_INT_PRIORITY);
+
+  /* Init the low level hardware */
+  HAL_MspInit();
+
+  /* Return function status */
+  return HAL_OK;
 }
 
